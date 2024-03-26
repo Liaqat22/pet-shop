3eb9ce7628709328c5296545b7b04757c2c0b706
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-03-26 07:40:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-// import Customers from './pages/Customers'
-// import Overview from './pages/Overview'
-// import Integration from './pages/Integration'
-// import Settings from './pages/Settings'
-// import Account from './pages/Account'
-// import Error from './pages/Error'
 import Layout from './layout/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import DogCards from './components/DogCards'
-// import Gernal from './pages/Gernal'
 
 
 function App() {
@@ -18,14 +11,6 @@ function App() {
     <>
     <Layout>
     <Routes>
-      {/* <Route path='/' element = {<Gernal/>}/> */}
-
-      {/* <Route path='/' element = {<Overview/>}/>
-      <Route path='/customers' element = {<Customers/>}/>
-      <Route path='/integration' element = {<Integration/>}/>
-      <Route path='/settings' element = {<Settings/>}/>
-      <Route path='/account' element = {<Account/>}/>
-      <Route path='/error' element = {<Error/>}/> */}
       <Route path='/' element = {<Home/>}/>
       <Route path='/about' element = {<About/>}/>
       <Route path='/pet' element = {<DogCards/>}/>
